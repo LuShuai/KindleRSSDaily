0ad1c7860553f2a6129f6b80ca28cead11cbff62
EC2 Default User <ec2-user@ip-10-185-173-158.ec2.internal>
2014-03-13 22:53:59 -0500

    config

--- server/send.py
+++ server/send.py
@@ -10,12 +10,17 @@ f = open("sources.txt", "r")
 feeds = f.readlines()
 f.close()
 
+p = open("/home/ec2-user/pass.txt", "r")
+s = p.readlines()
+p.close()
+
+
 dailykindle.build(feeds, '../temp/', timedelta(1))
 dailykindle.mobi('../temp/daily.opf', '../kindlegen/kindlegen')
 
 date = time.strftime("%m/%d/%Y")
 message = sendgrid.Mail()
-sg = sendgrid.SendGridClient('ludaxia', 'lssnqbbA1990726')
+sg = sendgrid.SendGridClient(s[0].strip('\n'), s[1].strip('\n'))
 message = sendgrid.Mail()
 message.add_to('sl988@kindle.com')
 message.set_subject('Daily RSS'+ date)
